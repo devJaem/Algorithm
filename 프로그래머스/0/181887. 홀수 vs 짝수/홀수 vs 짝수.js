d8270efde97a75af9fda860b8a3d06f9cfba9bc5
정재민 <wjdwoals9292@gmail.com>
2024-03-28 16:35:21 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,21 @@
+function solution(num_list) {
+    var answer = 0;
+    var oddSum = 0;
+    var evenSum = 0;
+    
+    for(var i=0; i<num_list.length; i++){
+        if(i%2===0){
+            evenSum += num_list[i];
+        }else if(i%2 ===1){
+            oddSum += num_list[i];
+        }
+    }
+    
+    if(evenSum >= oddSum){
+        answer = evenSum;
+    }else{
+        answer = oddSum;
+    }
+    
+    return answer;
+}
