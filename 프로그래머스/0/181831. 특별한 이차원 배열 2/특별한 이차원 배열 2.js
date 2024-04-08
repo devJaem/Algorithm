d4bddfa5b26531f18f6d18d5e296fdaab38e0d7c
정재민 <wjdwoals9292@gmail.com>
2024-04-08 17:22:31 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+    var answer = 1;
+    
+    for(var i=0; i<arr.length; i++){
+        for(var j=0; j<arr[i].length; j++){
+            if(arr[i][j] !== arr[j][i]){
+                answer = 0;
+                break;
+            }
+        }
+    }
+    return answer;
+}
