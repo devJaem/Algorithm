03437da1f8640e9f99b1097834d90990f0afdb89
정재민 <wjdwoals9292@gmail.com>
2024-05-29 10:56:27 +0900

    [level 2] Title: H-Index, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -4,10 +4,8 @@ function solution(citations) {
     citations.sort( (a, b) => b - a);
     
     for(let i=0; i<n; i++){
-        if(citations[i] >= i + 1){
-            answer = i + 1;
-        }else {
-            break;
+        if(i < citations[i]){
+            answer++;
         }
     }
     return answer;
