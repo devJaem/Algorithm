dd8e69ce59a1477495730b3eebc4c465930b873d
정재민 <wjdwoals9292@gmail.com>
2024-07-11 10:09:49 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.94 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,6 +1,5 @@
 function solution(arr1, arr2) {
     let answer = [];
-    
     for (let i = 0; i < arr1.length; i++) {
         let result = [];
         for (let j = 0; j < arr2[0].length; j++) {
