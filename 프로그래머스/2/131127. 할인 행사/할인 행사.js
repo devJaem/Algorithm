a4d33cb808efc562466490e23477642a82e263e8
정재민 <wjdwoals9292@gmail.com>
2024-07-12 09:29:52 +0900

    [level 2] Title: 할인 행사, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,21 +1,21 @@
 function solution(want, number, discount) {
- let result = 0
-    discount.forEach((v,i)=>{
-
-     let copy=[...discount].slice(i,i+10)
-     if(copy.length<10)return result
-
-     let flag=0
-     for(let j=0;j<want.length;j++){
-      if([...copy].filter(el=>el==want[j]).length==number[j]) 
-       flag++
-
-      else break
-     }
-
-     if(flag==want.length)result++
-
-  })
-
-    return result
+    let result = 0;
+    
+    for (let i = 0; i <= discount.length - 10; i++) {
+        let currentPeriod = discount.slice(i, i + 10);
+        let match = true;
+        
+        for (let j = 0; j < want.length; j++) {
+            let item = want[j];
+            let count = currentPeriod.filter(product => product === item).length;
+            if (count !== number[j]) {
+                match = false;
+                break;
+            }
+        }
+        if (match) {
+            result++;
+        }
+    }
+    return result;
 }
