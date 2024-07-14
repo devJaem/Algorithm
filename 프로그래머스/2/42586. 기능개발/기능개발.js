057a5aa0cda0d89e637761dbf6919ab0f7114632
정재민 <wjdwoals9292@gmail.com>
2024-07-14 21:22:48 +0900

    [level 2] Title: 기능개발, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,14 +1,11 @@
 function solution(progresses, speeds) {
     let answer = [];
     let days = [];
-
     for (let i = 0; i < progresses.length; i++) {
         days.push(Math.ceil((100 - progresses[i]) / speeds[i]));
     }
-
     let maxDay = days[0];
     let count = 1;
-
     for (let i = 1; i < days.length; i++) {
         if (days[i] <= maxDay) {
             count++;
@@ -18,8 +15,6 @@ function solution(progresses, speeds) {
             maxDay = days[i];
         }
     }
-
     answer.push(count);
-
     return answer;
 }
